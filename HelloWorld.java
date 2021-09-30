236c16c44aa31ed5ed7e55bddbad426fa9b51a08
Muhamad Rizki Mardiansyah <muhamadrizkimardiansyah137@gmail.com>
2021-09-30 19:15:01 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Muhamad Rizki Mardiansyah");
+		System.out.println("NIM : 6706213145");
+		System.out.println("Kelas : D3RPLA04");
+	}
+}	
